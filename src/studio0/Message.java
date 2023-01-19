fbef2838a0c29098f1d25ea1b01abf3b800a64ad
Andre <Andre@DESKTOP-8FIDMFL.dhcp.wustl.edu>
2023-01-19 12:10:49 -0600

    WHat up saul

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,6 +3,7 @@ package studio0;
 public class Message {
 
 	public static void main(String args[]) {
+		System.out.print("what up saul");
 		System.out.println("Say hi to your group mates!");
 	}
 }
